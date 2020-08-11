e79b22ae527d0acc107a60d0ad700ba7f4a0a3ce
Gwachukwu <gwachukwuelebeke@gmail.com>
2020-08-11 22:07:51 +0100

    make it better

--- controllers/User.js
+++ controllers/User.js
@@ -6,12 +6,12 @@ exports.signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
     if (!username || !email || !password) {
-      return res.status(400).json({ msg: "Please fill all fields" });
+      return res.status(400).json({ message: "Please fill all fields" });
     }
     //check if username already exists
     let user = await User.findOne({ username });
     if (user) {
-      return res.status(400).json({ msg: "user already exists" });
+      return res.status(400).json({ message: "user already exists" });
     }
     //check email format
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -26,7 +26,7 @@ exports.signUp = async (req, res, next) => {
     if (userEmail) {
       return res
         .status(400)
-        .json({ msg: "email already associated with an account" });
+        .json({ message: "email already associated with an account" });
     }
     user = new User({
       username,
@@ -63,11 +63,11 @@ exports.signIn = async (req, res) => {
   try {
     let user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ msg: "Incorrect email or password" });
+      return res.status(400).json({ message: "Incorrect email or password" });
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ msg: "Incorrect email or password" });
+      return res.status(400).json({ message: "Incorrect email or password" });
     }
     const payload = {
       user: {
